753d4a8145dc0e47fb761773fa70a3476e525106
yemmyharry <yemmyharry@gmail.com>
2022-02-23 17:23:16 +0100

    added other handlers

--- internal/adapter/api/resource/handler.go
+++ internal/adapter/api/resource/handler.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/yemmyharry/gas-inventory-service/internal/core/domain/resource"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -142,6 +143,54 @@ func (s *Server) GetItemDetail() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		reference := c.Param(strings.TrimSpace("reference"))
 		i, err := s.Inventory.GetItemDetail(reference)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, i)
+	}
+}
+
+func (s *Server) GetItemList() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		search_text := c.Param(strings.TrimSpace("search-text"))
+		page, _ := strconv.Atoi(c.Param(strings.TrimSpace("page")))
+		i, err := s.Inventory.GetItemsByMultipleSearch(search_text, page)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, i)
+	}
+}
+
+func (s *Server) GetItemListByOrgRef() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		search_text := c.Param(strings.TrimSpace("search-text"))
+		orgRef := c.Param(strings.TrimSpace("organization-reference"))
+		page, _ := strconv.Atoi(c.Param(strings.TrimSpace("page")))
+		i, err := s.Inventory.GetItemsByOrganisationSearch(orgRef, search_text, page)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, i)
+	}
+}
+
+func (s *Server) GetItemListByCatRefAndOrgRef() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		searchText := c.Param(strings.TrimSpace("search-text"))
+		orgRef := c.Param(strings.TrimSpace("organization-reference"))
+		catRef := c.Param(strings.TrimSpace("category-reference"))
+		page, _ := strconv.Atoi(c.Param(strings.TrimSpace("page")))
+		i, err := s.Inventory.GetItemsByCategoryAndOrganisationSearch(catRef, orgRef, searchText, page)
 		if err != nil {
 			c.JSON(500, gin.H{
 				"error": err.Error(),
