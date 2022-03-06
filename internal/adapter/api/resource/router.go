3b8fec7bbeb89166a9b541da8220be26a0392f0b
yemmyharry <yemmyharry@gmail.com>
2022-03-06 10:58:00 +0100

    made changes to routing parameters

--- internal/adapter/api/resource/router.go
+++ internal/adapter/api/resource/router.go
@@ -19,9 +19,10 @@ func (s *Server) Routes(router *gin.Engine) {
 	apirouter.PUT("/inventory/items/documents/:reference", s.AddDocument())
 	apirouter.PUT("/inventory/items/validate-item/:reference", s.ValidateItem())
 	apirouter.GET("/inventory/items/:reference", s.GetItemDetail())
-	apirouter.GET("/inventory/items/list/:search-text/list/page/:page", s.GetItemList())
+	apirouter.GET("/inventory/items/list/:search-text/page/:page", s.GetItemList())
 	apirouter.GET("/inventory/items/organisation/:organization-reference/:search-text/list/page/:page", s.GetItemListByOrgRef())
 	apirouter.GET("/inventory/items/category/organisation/:category-reference/:organization-reference/:search-text/list/page/:page", s.GetItemListByCatRefAndOrgRef())
 	apirouter.GET("/inventory/items/state/:state-reference/:search-text/list/page/:page", s.GetItemListByCityReference())
+	apirouter.GET("/inventory/items/category/state/:category-reference/:state-reference/:search-text/list/page/:page", s.GetItemListByCatRefAndCityRef())
 	router.NoRoute(func(c *gin.Context) { c.JSON(404, helper.PrintErrorMessage("404", shared.NoResourceFound)) })
 }
